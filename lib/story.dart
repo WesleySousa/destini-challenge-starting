95a05869300e8b4dcb0b93878d74b924d6c04bc7
Wesley Silva <wesley.sousa59@gmail.com>
2020-04-29 15:38:37 -0300

    complete app destini

--- lib/story.dart
+++ lib/story.dart
@@ -1,8 +1,7 @@
 class Story {
   String title;
-  String text;
   String choice1;
   String choice2;
 
-  Story(this.title, this.text, this.choice1, this.choice2);
+  Story({ this.title, this.choice1, this.choice2 });
 }
